f9b774ea66016a2749c024a6a9b14401c20fefb2
NeverForgetD <0327max@gmail.com>
2024-10-21 15:35:21 +0900

    add Fusion Package

--- QuickTrick/Assets/00_Splash/UIMainMenu.cs
+++ QuickTrick/Assets/00_Splash/UIMainMenu.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 namespace MainMenu
@@ -24,6 +25,16 @@ namespace MainMenu
             // 게임 시작 시 적용 할 것...예) PC의 경우 커서 보이게/안보이게
         }
 
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0) && !IsPointerOverUI())
+                LoadScene(1);
+        }
+
+        private bool IsPointerOverUI()
+        {
+            return EventSystem.current.IsPointerOverGameObject();
+        }
     }
 
 
