a95b04f48f2b98834bc8468d2f2e0c8b0db25cc2
NeverForgetD <0327max@gmail.com>
2024-12-01 00:37:43 +0900

    모든 에셋 이전 완료

---
+++ QuickTrick/Assets/02.Scripts/Common/Define.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+    public enum GameMode // 미니 게임 종류들
+    {
+        COLORSHIFT,
+        GUNFIGHT,
+        CARDGAME,
+        MaxCount, // 연산 편리상 넣은 것...실제로 게임 X
+    }
+
+    public enum RunnerStatus
+    {
+        NONE,
+        CONNECTINGSERVER,
+        WAITING,
+        TIMEOUT,
+        DISCONNECTING,
+        GAME,
+    }
+
+    public enum GameState
+    {
+        ENTER,
+        STARTGAME,
+        STARTROUND,
+    }
+}
---
+++ QuickTrick/Assets/02.Scripts/Legacy/Test.cs
@@ -0,0 +1,38 @@
+using Fusion;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using static Unity.Collections.Unicode;
+
+public class Test : MonoBehaviour
+{
+    public TextMeshProUGUI testText;
+    private NetworkRunner _runnerInstance;
+    private void InitInstance()
+    {
+        if (_runnerInstance == null)
+            _runnerInstance = FindObjectOfType<NetworkRunner>();
+    }
+
+    private void ShowRoomNumber()
+    {
+        InitInstance();
+        string text = $"Session name : {_runnerInstance.SessionInfo.Name}\n," +
+            $"player : {_runnerInstance.SessionInfo.PlayerCount}\n," +
+            $"player ID : {_runnerInstance.LocalPlayer.PlayerId}\n," +
+            $"IsServer : {_runnerInstance.IsServer}";
+        testText.text = text;
+
+    }
+
+    private void OnEnable()
+    {
+        //ShowRoomNumber();
+    }
+
+    private void Update()
+    {
+        //ShowRoomNumber();
+    }
+}
